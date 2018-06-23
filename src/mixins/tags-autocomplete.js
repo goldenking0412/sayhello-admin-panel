95f0b8e0251d2ef8b5c488925bb54584edee362b
chutuan <chutuan020693@gmail.com>
2018-06-24 00:21:09 +0700

    SH-147 fix load tags repeat multiple times

--- src/mixins/tags-autocomplete.js
+++ src/mixins/tags-autocomplete.js
@@ -1,20 +1,25 @@
 export default {
   data() {
     return {
-      autocompleteTags: []
+      autocompleteTags: [],
+      isTagEmpty: false
     }
   },
   methods: {
     loadAutocompleTags() {
       this.axios.get('/v5/admin/tags')
         .then((res) => {
+          if (res.data.tags.length == 0) {
+            this.isTagEmpty = true
+          }
           this.autocompleteTags = res.data.tags.map(tag => {return {text: tag } })
+
         })
         .catch((res) => {
         })
     },
     getAutocompleteTags(query) {
-      if (!this.autocompleteTags.length) {
+      if (!this.autocompleteTags.length && !this.isTagEmpty) {
         this.loadAutocompleTags()
       }
 
